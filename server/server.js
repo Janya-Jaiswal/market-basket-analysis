b1d3d33541db78c527a57b91b11570f6d4c44af5
janya <jaiswal@janyas-MacBook-Air.local>
2026-06-09 16:40:24 +0530

    created src

--- server/server.js
+++ server/server.js
@@ -1,10 +1,22 @@
 import express from "express";
+import cors from "cors";
+import helmet from "helmet";
+import morgan from "morgan";
+import compression from "compression";
+import keys from "./src/config/environmentVariables/secret"
+
 const app=express();
 app.use(express.json());
+app.use(cors());//enable cors for all origins
+app.use(morgan());
+app.use(compression());
+app.use(helmet());
+console.log(keys);
+
+app.get("/",(request,response)=>{response.status(200).json({"Message":"Server is running succesfully",
+    status: 200,
+    successful: true
+})});
 
 
-app.get("/",(request,response)=>{response.send("hello world")});
-app.get("/auth",(request,response)=>{response.send("you are authenticated")});
-app.get("/home",(request,response)=>{response.send("home page")});
-
-app.listen(8080,()=>{console.log("server is running")});
+app.listen(keys.port,()=>{console.log("server is running")});
---
+++ server/src/config/environmentVariables/secret.js
@@ -0,0 +1,6 @@
+import dotenv from "dotenv";
+dotenv.config();
+const keys={
+    port:process.env.PORT
+};
+export default keys;
