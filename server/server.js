62902d3ff8e500ae477b17a8230d3f5c9a1d818d
janya <jaiswal@janyas-MacBook-Air.local>
2026-06-10 15:34:39 +0530

    fixed

--- server/server.js
+++ server/server.js
@@ -3,12 +3,12 @@ import cors from "cors";
 import helmet from "helmet";
 import morgan from "morgan";
 import compression from "compression";
-import keys from "./src/config/environmentVariables/secret"
+import keys from "./src/config/environmentVariables/secret.js"
 
 const app=express();
 app.use(express.json());
 app.use(cors());//enable cors for all origins
-app.use(morgan());
+app.use(morgan("dev"));
 app.use(compression());
 app.use(helmet());
 console.log(keys);
